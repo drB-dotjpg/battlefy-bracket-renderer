a9ce5f04da8f46d0cf801a119deee0958108e535
Derek Bond <derek.bond24@gmail.com>
2022-08-19 22:34:24 -0500

    figuring out ts details

--- script.ts
+++ script.ts
@@ -1,55 +1,80 @@
-var tempEndpoint = "https://api.battlefy.com/stages/628ba4e331af073dcd3476da/matches";
-var games = [];
+interface Game {
+    topTeam: string;
+    topScore: number;
+    topWinner: boolean;
+
+    bottomTeam: string;
+    bottomScore: number;
+    bottomWinner: boolean;
+}
+
+const tempEndpoint = "https://api.battlefy.com/stages/628ba4e331af073dcd3476da/matches";
+let games: Game[] = [];
+
+
 fetch(tempEndpoint)
-    .then(function (response) {
+.then((response) => {
     return response.json();
 })
-    .then(function (bracketResponse) {
-    for (var i = 0; i < bracketResponse.length; i++) {
+.then((bracketResponse) => {
+    
+    for (var i = 0; i < bracketResponse.length; i++){
         games.push(jsonObjToGame(bracketResponse[i]));
     }
+
 });
-function jsonObjToGame(json) {
-    var game = {
+
+function jsonObjToGame(json): Game{
+    const game: Game = {
         topTeam: json.top.team.name,
         topScore: json.top.score,
         topWinner: json.top.winner,
         bottomTeam: json.bottom.team.name,
         bottomScore: json.bottom.score,
         bottomWinner: json.bottom.winner
-    };
+    }
     return game;
 }
-function gameToHTMLElement(game) {
-    var roundWrapper = document.createElement("div");
+
+function gameToHTMLElement(game: Game): HTMLDivElement{
+    const roundWrapper = document.createElement("div");
     roundWrapper.classList.add("round-wrapper");
-    var topTeamWrapper = document.createElement("div");
+
+    const topTeamWrapper = document.createElement("div");
     topTeamWrapper.classList.add("team-wrapper");
-    var topTeamName = document.createElement("div");
+
+    const topTeamName = document.createElement("div");
     topTeamName.classList.add("team");
     topTeamName.innerText = game.topTeam;
     topTeamWrapper.appendChild(topTeamName);
-    var topTeamScore = document.createElement("div");
+
+    const topTeamScore = document.createElement("div");
     topTeamScore.classList.add("score");
     topTeamScore.innerText = game.topScore.toString();
     topTeamWrapper.appendChild(topTeamScore);
+
     roundWrapper.appendChild(topTeamWrapper);
-    var bottomTeamWrapper = document.createElement("div");
+
+    const bottomTeamWrapper = document.createElement("div");
     bottomTeamWrapper.classList.add("team-wrapper");
-    var bottomTeamName = document.createElement("div");
+
+    const bottomTeamName = document.createElement("div");
     bottomTeamName.classList.add("team");
     bottomTeamName.innerText = game.bottomTeam;
     bottomTeamWrapper.appendChild(bottomTeamName);
-    var bottomTeamScore = document.createElement("div");
+
+    const bottomTeamScore = document.createElement("div");
     bottomTeamScore.classList.add("score");
     bottomTeamScore.innerText = game.bottomScore.toString();
     bottomTeamWrapper.appendChild(bottomTeamScore);
+
     roundWrapper.appendChild(bottomTeamWrapper);
-    if (game.topWinner) {
+
+    if (game.topWinner){
         topTeamWrapper.classList.add("winner");
-    }
-    else if (game.bottomWinner) {
+    } else if (game.bottomWinner) {
         bottomTeamWrapper.classList.add("winner");
     }
+
     return roundWrapper;
 }
