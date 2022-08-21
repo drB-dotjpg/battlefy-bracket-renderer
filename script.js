169b330eebae64f35ec225ea7b355e52b9762bfb
Derek Bond <derek.bond24@gmail.com>
2022-08-20 20:08:44 -0500

    major bugs squished

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-function generateOnClick() {
+var select = document.getElementById("bracket-select");
+select.addEventListener("change", function () {
+    var element = document.getElementById("bracket-zone");
+    element.innerHTML = "";
+    var bracketId = select.value;
+    endpointToHTMLElements(bracketId, element);
+});
+function searchForBrackets() {
     var inputVal = document.getElementById('input').value;
-    var bracketZone = document.getElementById('bracket-zone');
-    bracketZone.innerHTML = "";
-    endpointToHTMLElements(inputVal, bracketZone);
+    select.innerHTML = "";
+    var inputSplit = inputVal.split('/');
+    var splitLen = inputSplit.length;
+    var tourneyId = inputSplit[splitLen - 1].indexOf("info") !== -1 || inputSplit[splitLen - 1] == ""
+        ? inputSplit[splitLen - 2]
+        : inputSplit[splitLen - 1];
+    document.getElementById("bracket-zone").innerHTML = "";
+    fetch("https://api.battlefy.com/tournaments/".concat(tourneyId, "?extend%5Bcampaign%5D%5Bsponsor%5D=true&extend%5Bstages%5D%5B%24query%5D%5BdeletedAt%5D%5B%24exists%5D=false&extend%5Bstages%5D%5B%24opts%5D%5Bname%5D=1&extend%5Bstages%5D%5B%24opts%5D%5Bbracket%5D=1"))
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (tourneyResponse) {
+        for (var i = 0; i < tourneyResponse[0].stages.length; i++) {
+            if (tourneyResponse[0].stages[i].bracket.type == "elimination") {
+                var option = document.createElement("option");
+                option.value = tourneyResponse[0].stages[i]._id;
+                option.innerText = tourneyResponse[0].stages[i].name;
+                select.appendChild(option);
+            }
+        }
+        select.dispatchEvent(new Event('change'));
+        gsap.fromTo("#options-pt2", { display: "flex", opacity: 0, scale: .75 }, { opacity: 1, scale: 1 });
+    });
 }
 function endpointToHTMLElements(endpoint, element) {
+    element.innerHTML = '';
     fetch("https://api.battlefy.com/stages/".concat(endpoint, "/matches"))
         .then(function (response) {
         return response.json();
     })
         .then(function (bracketResponse) {
         var winnersGames = [];
-        var winnersHighestRound = 0;
+        var winnersHighestRound = 1;
         var losersGames = [];
-        var losersHighestRound = 0;
+        var losersHighestRound = 1;
         for (var i = 0; i < bracketResponse.length; i++) {
             var game = jsonObjToGame(bracketResponse[i]);
             if (!game.losers) {
@@ -36,7 +64,7 @@ function endpointToHTMLElements(endpoint, element) {
             grid.classList.add("grid-wrapper");
             var gridHeader = document.createElement("div");
             gridHeader.classList.add("grid-header");
-            gridHeader.innerText = "Round ".concat(i + 1);
+            gridHeader.innerText = getRoundName(i + 1, winnersRounds.length, true);
             grid.appendChild(gridHeader);
             for (var j = 0; j < winnersRounds[i].length; j++) {
                 grid.appendChild(gameToHTMLElement(winnersRounds[i][j]));
@@ -44,41 +72,72 @@ function endpointToHTMLElements(endpoint, element) {
             winnersBracketElement.appendChild(grid);
         }
         var losersBracketElement = document.createElement("div");
-        losersBracketElement.classList.add("bracket-wrapper");
-        var losersRounds = Array.apply(null, Array(losersHighestRound)).map(function () { return []; });
-        for (var i = 0; i < losersGames.length; i++) {
-            losersRounds[losersGames[i].round - 1].push(losersGames[i]);
-        }
-        for (var i = 0; i < losersRounds.length; i++) {
-            var grid = document.createElement("div");
-            grid.classList.add("grid-wrapper");
-            var gridHeader = document.createElement("div");
-            gridHeader.classList.add("grid-header");
-            gridHeader.innerText = "Round ".concat(i + 1);
-            grid.appendChild(gridHeader);
-            for (var j = 0; j < losersRounds[i].length; j++) {
-                grid.appendChild(gameToHTMLElement(losersRounds[i][j]));
+        if (losersGames.length > 0) {
+            losersBracketElement.classList.add("bracket-wrapper");
+            var losersRounds = Array.apply(null, Array(losersHighestRound)).map(function () { return []; });
+            var isThirdPlace = false;
+            if (losersGames.length == 1) {
+                losersRounds[losersGames[0].round].push(losersGames[0]);
+                isThirdPlace = true;
             }
-            losersBracketElement.appendChild(grid);
+            else {
+                for (var i = 0; i < losersGames.length; i++) {
+                    losersRounds[losersGames[i].round - 1].push(losersGames[i]);
+                }
+            }
+            for (var i = 0; i < losersRounds.length; i++) {
+                var grid = document.createElement("div");
+                grid.classList.add("grid-wrapper");
+                var gridHeader = document.createElement("div");
+                gridHeader.classList.add("grid-header");
+                if (isThirdPlace) {
+                    gridHeader.innerText = "Third Place";
+                }
+                else {
+                    gridHeader.innerText = getRoundName(i + 1, losersRounds.length, false);
+                }
+                grid.appendChild(gridHeader);
+                for (var j = 0; j < losersRounds[i].length; j++) {
+                    grid.appendChild(gameToHTMLElement(losersRounds[i][j]));
+                }
+                losersBracketElement.appendChild(grid);
+            }
         }
         element.appendChild(winnersBracketElement);
         if (losersGames.length > 0) {
             element.appendChild(losersBracketElement);
         }
-        gsap.fromTo(".round-wrapper", { scale: 0 }, { scale: 1, stagger: { amount: 1.5, grid: "auto", from: "start" } });
-        gsap.fromTo(".grid-header", { opacity: 0 }, { opacity: 1, stagger: { amount: 1.5, grid: "auto", from: "start" } });
+        gsap.fromTo(".round-wrapper", { opacity: 0, scale: .8 }, { opacity: 1, scale: 1, stagger: { amount: .3, grid: "auto", from: "start" } });
+        gsap.fromTo(".grid-header", { opacity: 0 }, { opacity: 1, stagger: { amount: .3, grid: "auto", from: "start" } });
     });
 }
 function jsonObjToGame(json) {
+    var topTeam = json.top.team;
+    if (topTeam === undefined) {
+        topTeam = { name: "-" };
+    }
+    var topScore = json.top.score;
+    if (topScore === undefined) {
+        topScore = "-";
+    }
+    var bottomTeam = json.bottom.team;
+    if (bottomTeam === undefined) {
+        bottomTeam = { name: "-" };
+    }
+    var bottomScore = json.bottom.score;
+    if (bottomScore === undefined) {
+        bottomScore = "-";
+    }
+    // console.log(topName,topScore,bottomName,bottomScore);
     var game = {
         match: json.matchNumber,
         round: json.roundNumber,
         losers: json.matchType == "loser",
-        topTeam: json.top.team.name,
-        topScore: json.top.score,
+        topTeam: topTeam.name,
+        topScore: topScore,
         topWinner: json.top.winner,
-        bottomTeam: json.bottom.team.name,
-        bottomScore: json.bottom.score,
+        bottomTeam: bottomTeam.name,
+        bottomScore: bottomScore,
         bottomWinner: json.bottom.winner
     };
     return game;
@@ -115,4 +174,18 @@ function gameToHTMLElement(game) {
         bottomTeamWrapper.classList.add("winner");
     }
     return roundWrapper;
+}
+function getRoundName(round, numRounds, isDoubleElim) {
+    if (isDoubleElim) {
+        if (round == numRounds) {
+            return "Reset";
+        }
+        else if (round == numRounds - 1) {
+            return "Finals";
+        }
+    }
+    else if (round == numRounds) {
+        return "Finals";
+    }
+    return "Round " + round;
 }
--- script.ts
+++ script.ts
@@ -1,5 +1,14 @@
 declare var gsap: any;
 
+const select = document.getElementById("bracket-select");
+
+select.addEventListener("change", function() {
+    const element = document.getElementById("bracket-zone");
+    element.innerHTML = "";
+    const bracketId = (<HTMLSelectElement>select).value;
+    endpointToHTMLElements(bracketId, element);
+});
+
 interface Game {
     match: number;
     round: number;
@@ -14,14 +23,41 @@ interface Game {
     bottomWinner: boolean;
 }
 
-function generateOnClick(){
+function searchForBrackets(){
     const inputVal = (<HTMLInputElement>document.getElementById('input')).value;
-    const bracketZone = document.getElementById('bracket-zone');
-    bracketZone.innerHTML = "";
-    endpointToHTMLElements(inputVal, bracketZone);
+    select.innerHTML = "";
+
+    const inputSplit = inputVal.split('/');
+    const splitLen = inputSplit.length;
+    const tourneyId = inputSplit[splitLen-1].indexOf("info") !== -1 || inputSplit[splitLen-1] == "" 
+        ? inputSplit[splitLen-2] 
+        : inputSplit[splitLen-1];
+
+    document.getElementById("bracket-zone").innerHTML = "";
+
+    fetch(`https://api.battlefy.com/tournaments/${tourneyId}?extend%5Bcampaign%5D%5Bsponsor%5D=true&extend%5Bstages%5D%5B%24query%5D%5BdeletedAt%5D%5B%24exists%5D=false&extend%5Bstages%5D%5B%24opts%5D%5Bname%5D=1&extend%5Bstages%5D%5B%24opts%5D%5Bbracket%5D=1`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((tourneyResponse) => {
+        
+        for(var i = 0; i < tourneyResponse[0].stages.length; i++){
+            if (tourneyResponse[0].stages[i].bracket.type == "elimination"){
+                const option = document.createElement("option");
+                option.value = tourneyResponse[0].stages[i]._id;
+                option.innerText = tourneyResponse[0].stages[i].name;
+                select.appendChild(option);
+            }
+        }
+        select.dispatchEvent(new Event('change'));
+
+        gsap.fromTo("#options-pt2", {display: "flex", opacity: 0, scale: .75}, {opacity: 1, scale: 1});
+    })
 }
 
 function endpointToHTMLElements(endpoint: string, element: HTMLElement){
+    element.innerHTML = '';
+
     fetch(`https://api.battlefy.com/stages/${endpoint}/matches`)
     .then((response) => {
         return response.json();
@@ -29,9 +65,9 @@ function endpointToHTMLElements(endpoint: string, element: HTMLElement){
     .then((bracketResponse) => {
         
         let winnersGames: Game[] = [];
-        let winnersHighestRound: number = 0;
+        let winnersHighestRound: number = 1;
         let losersGames: Game[] = [];
-        let losersHighestRound: number = 0;
+        let losersHighestRound: number = 1;
         
         for (var i = 0; i < bracketResponse.length; i++){
             const game: Game = jsonObjToGame(bracketResponse[i]);
@@ -59,7 +95,7 @@ function endpointToHTMLElements(endpoint: string, element: HTMLElement){
 
             const gridHeader = document.createElement("div");
             gridHeader.classList.add("grid-header");
-            gridHeader.innerText = `Round ${i+1}`;
+            gridHeader.innerText = getRoundName(i+1, winnersRounds.length, true);
             grid.appendChild(gridHeader);
 
             for (var j = 0; j < winnersRounds[i].length; j++){
@@ -69,29 +105,45 @@ function endpointToHTMLElements(endpoint: string, element: HTMLElement){
             winnersBracketElement.appendChild(grid);
         }
 
-
         const losersBracketElement = document.createElement("div");
-        losersBracketElement.classList.add("bracket-wrapper");
-        const losersRounds = Array.apply(null, Array(losersHighestRound)).map(()=>{return []});
 
-        for (var i = 0; i < losersGames.length; i++){
-            losersRounds[losersGames[i].round-1].push(losersGames[i]);
-        }
+        if (losersGames.length > 0){
 
-        for (var i = 0; i < losersRounds.length; i++){
-            const grid = document.createElement("div");
-            grid.classList.add("grid-wrapper");
+            losersBracketElement.classList.add("bracket-wrapper");
+            const losersRounds = Array.apply(null, Array(losersHighestRound)).map(()=>{return []});
 
-            const gridHeader = document.createElement("div");
-            gridHeader.classList.add("grid-header");
-            gridHeader.innerText = `Round ${i+1}`;
-            grid.appendChild(gridHeader);
+            var isThirdPlace: boolean = false;
 
-            for (var j = 0; j < losersRounds[i].length; j++){
-                grid.appendChild(gameToHTMLElement(losersRounds[i][j]));
+            if (losersGames.length == 1){
+                losersRounds[losersGames[0].round].push(losersGames[0]);
+                isThirdPlace = true;
+            } else {
+                for (var i = 0; i < losersGames.length; i++){
+                    losersRounds[losersGames[i].round - 1].push(losersGames[i]);
+                }
             }
 
-            losersBracketElement.appendChild(grid);
+            for (var i = 0; i < losersRounds.length; i++){
+                const grid = document.createElement("div");
+                grid.classList.add("grid-wrapper");
+
+                const gridHeader = document.createElement("div");
+                gridHeader.classList.add("grid-header");
+                
+                if (isThirdPlace){
+                    gridHeader.innerText = `Third Place`;
+                } else {
+                    gridHeader.innerText = getRoundName(i+1, losersRounds.length, false);
+                }
+                
+                grid.appendChild(gridHeader);
+
+                for (var j = 0; j < losersRounds[i].length; j++){
+                    grid.appendChild(gameToHTMLElement(losersRounds[i][j]));
+                }
+
+                losersBracketElement.appendChild(grid);
+            }
         }
 
         element.appendChild(winnersBracketElement);
@@ -99,24 +151,43 @@ function endpointToHTMLElements(endpoint: string, element: HTMLElement){
             element.appendChild(losersBracketElement);
         }
 
-        gsap.fromTo(".round-wrapper", {scale: 0}, {scale: 1, stagger:{amount: 1.5, grid: "auto", from: "start"}});
-        gsap.fromTo(".grid-header", {opacity: 0}, {opacity: 1, stagger:{amount: 1.5, grid: "auto", from: "start"}});
+        gsap.fromTo(".round-wrapper", {opacity: 0, scale: .8}, {opacity: 1, scale: 1, stagger:{amount: .3, grid: "auto", from: "start"}});
+        gsap.fromTo(".grid-header", {opacity: 0}, {opacity: 1, stagger:{amount: .3, grid: "auto", from: "start"}});
 
     });
 }
 
 function jsonObjToGame(json): Game{
+    var topTeam = json.top.team;
+    if (topTeam === undefined) {
+        topTeam = {name: "-"}
+    }
+    var topScore = json.top.score;
+    if (topScore === undefined){
+        topScore = "-";
+    }
+    var bottomTeam = json.bottom.team;
+    if (bottomTeam === undefined) {
+        bottomTeam = {name: "-"}
+    }
+    var bottomScore = json.bottom.score;
+    if (bottomScore === undefined) { 
+        bottomScore = "-";
+    }
+
+    // console.log(topName,topScore,bottomName,bottomScore);
+
     const game: Game = {
         match: json.matchNumber,
         round: json.roundNumber,
         losers: json.matchType == "loser",
-        topTeam: json.top.team.name,
-        topScore: json.top.score,
+        topTeam: topTeam.name,
+        topScore: topScore,
         topWinner: json.top.winner,
-        bottomTeam: json.bottom.team.name,
-        bottomScore: json.bottom.score,
+        bottomTeam: bottomTeam.name,
+        bottomScore: bottomScore,
         bottomWinner: json.bottom.winner
-    }
+    };
     return game;
 }
 
@@ -161,4 +232,17 @@ function gameToHTMLElement(game: Game): HTMLDivElement{
     }
 
     return roundWrapper;
+}
+
+function getRoundName(round: number, numRounds: number, isDoubleElim: boolean): string{
+    if (isDoubleElim){
+        if (round == numRounds){
+            return "Reset";
+        } else if (round == numRounds - 1){
+            return "Finals";
+        }
+    } else if (round == numRounds){
+        return "Finals";
+    }
+    return "Round " + round;
 }
